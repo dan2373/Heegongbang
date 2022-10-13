8635846f3a8a3bf5b0a133d9d1f6bfec9400e640
Dan:HARU <78343345+dan2373@users.noreply.github.com>
2022-10-13 20:13:15 +0900

    MALL

---
+++ data/cache/theme/buzinga54/footerinfo.php
@@ -0,0 +1,9 @@
+<?php
+if (!defined('_GNUBOARD_')) exit;
+
+$footerinfo=array (
+  'tel' => '01065737279',
+  'etc' => '정성 담은 작은 예쁨으로 일상에 행복을 더하는 손수만듦_희공방입니다 :)',
+  'depositor' => '강대희',
+  'account' => '신한은행 110-324-147100',
+);
